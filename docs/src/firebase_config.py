5d8d391bbd4b21d77424d95ac5e086815feb7e2e
Tobias Viana Araujo <tobias.araujo@sou.inteli.edu.br>
2025-10-14 00:25:53 -0300

    feat: adiciona toda a estrutura de autentificacao

---
+++ docs/src/firebase_config.py
@@ -0,0 +1,10 @@
+# This file contains the firebase configuration details.
+firebaseConfig = {
+  "apiKey": "AIzaSyDrXfRBC_QlFc4ez0wQivOYoR5DiI2Aobo",
+  "authDomain": "agrotech---effatha.firebaseapp.com",
+  "projectId": "agrotech---effatha",
+  "storageBucket": "agrotech---effatha.firebasestorage.app",
+  "messagingSenderId": "72149352701",
+  "appId": "1:72149352701:web:9fcb7f5ffcd5341f3dbe94",
+  "measurementId": "G-VPQ4ETYKVY"
+};
